34eda70f7913697a7eac9df0149d762d7101443d
Olivier Demers <olidem123@gmail.com>
2020-10-26 00:49:51 -0400

    Create tiles prefab + script

---
+++ Assets/Scripts/Game/Tile/Tile.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private Material hoveredMaterial = null;
+    [SerializeField] private Material activeMaterial = null;
+
+    private bool _isActive;
+    
+    private MeshRenderer _renderer;
+
+    private void Start()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+        
+    }
+
+    public void SetActive(bool active)
+    {
+        _isActive = active;
+        SetState(_isActive ? State.Active : State.Hidden);
+    }
+
+    private void OnMouseEnter()
+    {
+        if (_isActive)
+        {
+            SetState(State.Hover);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (_isActive)
+        {
+            SetState(State.Active);
+        }
+    }
+
+    private void SetState(State state)
+    {
+        switch (state)
+        {
+            case State.Hidden:
+                gameObject.SetActive(false);
+                break;
+            case State.Active:
+                gameObject.SetActive(true);
+                _renderer.material = activeMaterial;
+                break;
+            case State.Hover:
+                gameObject.SetActive(true);
+                _renderer.material = hoveredMaterial;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private enum State
+    {
+        Hidden, Active, Hover 
+    }
+}
