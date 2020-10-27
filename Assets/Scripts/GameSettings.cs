f721f5b50c64207db1492f22b82bdd297676c877
Olivier Demers <olidem123@gmail.com>
2020-10-27 11:36:06 -0400

    Added epic menu

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameSettings
+{
+
+    public static PlayerType Player1;
+    public static PlayerType Player2;
+    
+    public enum PlayerType
+    {
+        Player, EasyAI, HardAI
+    }
+}
---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    [SerializeField] private TMP_Dropdown player1Dropdown;
+    [SerializeField] private TMP_Dropdown player2Dropdown;
+
+
+    public void StartGame()
+    {
+        GameSettings.Player1 = GetPlayerType(player1Dropdown.value);
+        GameSettings.Player2 = GetPlayerType(player2Dropdown.value);
+        SceneManager.LoadScene("GameScene");
+    }
+
+    private static GameSettings.PlayerType GetPlayerType(int value)
+    {
+        switch (value)
+        {
+            case 0:
+                return GameSettings.PlayerType.Player;
+            case 1:
+                return GameSettings.PlayerType.EasyAI;
+            case 2:
+                return GameSettings.PlayerType.HardAI;
+        }
+
+        return GameSettings.PlayerType.Player;
+    }
+    
+}
